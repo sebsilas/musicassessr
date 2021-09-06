5c769d573148f8af0c7212e9287990b1271b696d
mcetn <med.etteyeb@hotmail.com>
2021-09-06 10:11:18 +0100

    c
    Former-commit-id: 59599f32f552fad97d5fe9b09dfc5a5f2e51d5ee [formerly 61fca51566897af1d06dd5983d4774186fd0b704]
    Former-commit-id: 50e285595833e84356d64b16011e868c1c0dbbd0
    Former-commit-id: af7ec8e95412c3788e7940a4d490a5c5f08df222

--- R/record_audio_page.R
+++ R/record_audio_page.R
@@ -34,6 +34,7 @@ record_audio_page <- function(body = NULL, label = "record_audio_page", stimuli 
     shiny::tags$head(
 
       auto_next_page(auto_next_page),
+      shiny::includeScript(system.file("static-website-s3/spinner/spin.js", package = "musicassessr")),
 
       shiny::tags$script(set_answer_meta_data(answer_meta_data)),
         #htmltools::HTML('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">')

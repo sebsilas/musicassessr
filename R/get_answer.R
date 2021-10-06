c1e461553608fe29c3545d9b6d134a834c080674
syntheso <sebsilas@gmail.com>
2021-10-06 13:24:49 +0200

    x
    Former-commit-id: ff61e28783e0bca2ac84cd1d3505e8e5d92c7680 [formerly 7a6cd69c46386fcbc06933e062199a0ee776502c]
    Former-commit-id: e5d397affb31b357876a5d7f36032a69dacdd762
    Former-commit-id: 8c2c2ad11087e689b8f85cc135a3721b33364f4c

--- R/get_answer.R
+++ R/get_answer.R
@@ -22,7 +22,7 @@ melconv <- function(file_name, return_notes_and_durs = TRUE) {
   res <- res[length(res)]
   res <- strsplit(res, ".", fixed = TRUE)[[1]][1]
   # res <- paste0('/Users/sebsilas/musicassessr/', res, '.mid')
-  res <- paste0('/srv/shiny-server/files/mid/', res, '.mid')
+  res <- paste0('srv/shiny-server/files/mid/', res, '.mid')
 
   if(return_notes_and_durs) {
     itembankr::midi_file_to_notes_and_durations(res)

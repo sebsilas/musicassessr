ccd6b7fda4fe5d3536197dba77d4a375ffe9ab8a
syntheso <sebsilas@gmail.com>
2021-10-13 11:09:26 +0100

    x
    Former-commit-id: b47d2fb46afc7e5e2a85a4a555acd00910f06bcb [formerly b65957479f2fa33fa756e7f9f01615cfa8bc8b46]
    Former-commit-id: 62b9e656eacb1c4de66d339a5ac1ac8e9a2e0614
    Former-commit-id: b068bc0419e7af30eeb7fa6a51482c7735f3448a

--- data-raw/insts.R
+++ data-raw/insts.R
@@ -5,4 +5,5 @@ insts <- insts_table$dict_key
 insts_dict <- lapply(insts, psychTestR::i18n)
 
 usethis::use_data(insts, insts_table, insts_dict, overwrite = TRUE)
+usethis::use_data(insts, insts_table, insts_dict, overwrite = TRUE, internal = TRUE)
 

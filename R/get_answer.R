012e1644888af4df39db1d93fd3eb2c4e5309b5a
syntheso <sebsilas@gmail.com>
2021-11-04 20:21:29 +0100

    pyin -> remote version
    Former-commit-id: cccab143fa5b54ac7f8b25166bacb3490362cbe5 [formerly 2f9620282036c028a31c80aba73a804410ee5250]
    Former-commit-id: e7d1cd0c3fe60a521b85e14e32bbc3f4b57458e0
    Former-commit-id: 89053e741157afb42a05c19c687ed933668cef1d

--- R/get_answer.R
+++ R/get_answer.R
@@ -182,81 +182,81 @@ pyin <- function(file_name, transform_file = NULL,
 }
 
 
-
-pyin <- function(file_name, transform_file = NULL,
-                 normalise = FALSE, hidePrint = TRUE, type = "notes") {
-
-  #file_name <- '/Users/sebsilas/true.wav'
-  #file_name <- '/Users/sebsilas/Downloads/13-8-2021--17-8--18.wav'
-
-
-  if(type == "pitch_track") {
-    vamp_cmd <- "vamp:pyin:pyin:smoothedpitchtrack"
-  } else if(type == "notes") {
-    vamp_cmd <- "vamp:pyin:pyin:notes"
-  } else {
-    stop("Unknown type")
-  }
-
-  if(is.null(transform_file)) {
-    args <- c("-d",
-              vamp_cmd,
-              file_name,
-              "-w",
-              "csv --csv-stdout")
-  } else {
-    args <- c(paste0('-t ', transform_file),
-              file_name,
-              "-w",
-              "csv --csv-stdout")
-  }
-
-  if(normalise == 1) {
-    args <- c(args, "--normalise")
-  }
-
-  if(hidePrint) {
-    sa_out <- system2(command = "/Users/sebsilas/sonic-annotator",
-                      args = args,
-                      stdout = TRUE, stderr = FALSE)
-  } else {
-    sa_out <- system2(command = "/Users/sebsilas/sonic-annotator",
-                      args = args,
-                      stdout = TRUE)
-  }
-
-  if(length(sa_out) == 0) {
-    if(type == "notes") {
-      res <- tibble::tibble(onset = NA, dur = NA, freq = NA, note = NA, file_name = file_name)
-    } else {
-      res <- tibble::tibble(onset = NA, freq = NA, file_name = file_name)
-    }
-  } else {
-    res <- read.csv(text = sa_out, header = FALSE)
-
-    if(type == "notes") {
-      res <- res %>%
-        dplyr::rename(onset = V2, dur = V3, freq = V4) %>%
-        dplyr::mutate(
-          onset = round(onset, 2),
-          dur = round(dur, 2),
-          freq = round(freq, 2),
-          note = round(hrep::freq_to_midi(freq)))
-    } else {
-      res <- res %>%
-        dplyr::rename(onset = V2, freq = V3) %>%
-        dplyr::mutate(
-          onset = round(onset, 2),
-          freq = round(freq, 2))
-    }
-
-    file_name <- res$V1[[1]]
-
-    res <- res %>% dplyr::select(-V1)
-
-    res <- tibble::tibble(file_name, res)
-  }
-}
+#
+# pyin <- function(file_name, transform_file = NULL,
+#                  normalise = FALSE, hidePrint = TRUE, type = "notes") {
+#
+#   #file_name <- '/Users/sebsilas/true.wav'
+#   #file_name <- '/Users/sebsilas/Downloads/13-8-2021--17-8--18.wav'
+#
+#
+#   if(type == "pitch_track") {
+#     vamp_cmd <- "vamp:pyin:pyin:smoothedpitchtrack"
+#   } else if(type == "notes") {
+#     vamp_cmd <- "vamp:pyin:pyin:notes"
+#   } else {
+#     stop("Unknown type")
+#   }
+#
+#   if(is.null(transform_file)) {
+#     args <- c("-d",
+#               vamp_cmd,
+#               file_name,
+#               "-w",
+#               "csv --csv-stdout")
+#   } else {
+#     args <- c(paste0('-t ', transform_file),
+#               file_name,
+#               "-w",
+#               "csv --csv-stdout")
+#   }
+#
+#   if(normalise == 1) {
+#     args <- c(args, "--normalise")
+#   }
+#
+#   if(hidePrint) {
+#     sa_out <- system2(command = "/Users/sebsilas/sonic-annotator",
+#                       args = args,
+#                       stdout = TRUE, stderr = FALSE)
+#   } else {
+#     sa_out <- system2(command = "/Users/sebsilas/sonic-annotator",
+#                       args = args,
+#                       stdout = TRUE)
+#   }
+#
+#   if(length(sa_out) == 0) {
+#     if(type == "notes") {
+#       res <- tibble::tibble(onset = NA, dur = NA, freq = NA, note = NA, file_name = file_name)
+#     } else {
+#       res <- tibble::tibble(onset = NA, freq = NA, file_name = file_name)
+#     }
+#   } else {
+#     res <- read.csv(text = sa_out, header = FALSE)
+#
+#     if(type == "notes") {
+#       res <- res %>%
+#         dplyr::rename(onset = V2, dur = V3, freq = V4) %>%
+#         dplyr::mutate(
+#           onset = round(onset, 2),
+#           dur = round(dur, 2),
+#           freq = round(freq, 2),
+#           note = round(hrep::freq_to_midi(freq)))
+#     } else {
+#       res <- res %>%
+#         dplyr::rename(onset = V2, freq = V3) %>%
+#         dplyr::mutate(
+#           onset = round(onset, 2),
+#           freq = round(freq, 2))
+#     }
+#
+#     file_name <- res$V1[[1]]
+#
+#     res <- res %>% dplyr::select(-V1)
+#
+#     res <- tibble::tibble(file_name, res)
+#   }
+# }
 
 
 #pyin('/Users/sebsilas/true.wav')

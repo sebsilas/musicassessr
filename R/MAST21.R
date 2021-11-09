0d5617a7607ee922404cfe0e09d9740587bbb636
syntheso <sebsilas@gmail.com>
2021-11-09 14:45:56 +0100

    production
    Former-commit-id: ea7826523b33c4f4af2ed2217fac183a0d81fd4d [formerly 4c4dfa645a5a91461f4652c3d18552d9a35ff631]
    Former-commit-id: a254f007eb84392236df7dd8101eaca57c867ce2
    Former-commit-id: f154178b14d3873273f57af9c392393ad7ee4e27

--- R/MAST21.R
+++ R/MAST21.R
@@ -233,7 +233,7 @@ sing_happy_birthday_page <- function(feedback = FALSE) {
 #' @export
 #'
 #' @examples
-MAST <- function() {
+MAST <- function(state = "production") {
 
   make_aws_credentials_global(list(api_url = "https://255uxe6ajl.execute-api.us-east-1.amazonaws.com/api",
                                    bucket_name = "shinny-app-source-41630",
@@ -250,7 +250,7 @@ MAST <- function() {
                               bucket_region = "us-east-1",
                               identity_pool_id = "us-east-1:feecdf7e-cdf6-416f-94d0-a6de428c8c6b",
                               destination_bucket = "shinny-app-destination-41630"),
-      musicassessr_state("local")
+      musicassessr_state(state)
       )),
 
     psychTestR::one_button_page(tags$div(

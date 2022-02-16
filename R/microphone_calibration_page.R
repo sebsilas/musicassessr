a3b4708eacd228cc1c94ee2b0331506c57b92a7d
syntheso <sebsilas@gmail.com>
2022-02-16 17:19:04 +0100

    Typo

--- R/microphone_calibration_page.R
+++ R/microphone_calibration_page.R
@@ -18,7 +18,7 @@ microphone_calibration_page <- function(button_text = "Start test") {
   ui <- shiny::tags$div(
 
     shiny::tags$h2(psychTestR::i18n("Microphone_Test")),
-    shiny::tags$p(psychtestR::i18n("meter")),
+    shiny::tags$p(psychTestR::i18n("meter")),
     shiny::tags$meter(id = "volumeMeter", high = "0.25", max = "1", value = "0", style = "width: 400px; height: 50px; margin: 20px 0 20px 0;"),
     shiny::tags$div(shiny::tags$p(psychTestR::i18n("microphone_calibration_message_1")),
                    shiny::tags$p(psychTestR::i18n("microphone_calibration_message_2")),

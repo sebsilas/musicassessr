309a41f8ee98d67fcd35500a60d470a340c8ee7a
syntheso <sebsilas@gmail.com>
2022-03-30 11:57:08 +0100

    Span work around

--- R/sample_from_item_bank.R
+++ R/sample_from_item_bank.R
@@ -208,16 +208,19 @@ sample_arrhythmic <- function(item_bank, num_items_arrhythmic, id = "arrhythmic_
 
 sample_rhythmic <- function(item_bank, num_items_rhythmic, id = "rhythmic_melody") {
   psychTestR::code_block(function(state, ...) {
-    cat(file=stderr(), "\n")
+    #cat(file=stderr(), "\n")
     span <- psychTestR::get_global("span", state)
-    cat("span: \n")
+    if(is.null(span) | span < 10) {
+      span <- 10
+    }
+    #cat("span: \n")
     cat(file=stderr(), str(span), "\n")
     span_warning(span)
     # sample rhythmic
     rhythmic_item_bank_subset <- itembankr::subset_item_bank(item_bank = item_bank, span_max = span)
-    cat(file=stderr(), str(rhythmic_item_bank_subset), "\n")
+    #cat(file=stderr(), str(rhythmic_item_bank_subset), "\n")
     rhythmic_sample <- musicassessr::item_sampler(rhythmic_item_bank_subset, num_items_rhythmic)
-    cat(file=stderr(), str(rhythmic_sample), "\n")
+    #cat(file=stderr(), str(rhythmic_sample), "\n")
     psychTestR::set_global(id, rhythmic_sample, state)
   })
 }

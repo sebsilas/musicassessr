54607c3f6ec95c426012fffcd120990eb7264c67
syntheso <sebsilas@gmail.com>
2022-05-17 17:13:15 +0200

    x

--- R/js.R
+++ R/js.R
@@ -69,6 +69,7 @@ create_app_from_template <- function(dir) {
 #' @param state
 #' @param visual_notation
 #' @param midi_file_playback
+#' @param copy_app_to_location
 #'
 #' @return
 #' @export
@@ -76,7 +77,10 @@ create_app_from_template <- function(dir) {
 #' @examples
 musicassessr_js <- function(musicassessr_aws = FALSE,
                             visual_notation = FALSE,
-                            midi_file_playback = FALSE) {
+                            midi_file_playback = FALSE,
+                            copy_audio_to_location) {
+
+  create_app_from_template(copy_audio_to_location)
 
   # TODO add midi_input argument. This would make importing https://cdn.jsdelivr.net/npm/webmidi@2.5.1 and getMIDIin.js optional
   c(
@@ -134,45 +138,3 @@ get_musicassessr_state_js_script <- function(musicassessr_aws = FALSE) {
 }
 
 
-enable.cors <- '
-// Create the XHR object.
-function createCORSRequest(method, url) {
-var xhr = new XMLHttpRequest();
-if ("withCredentials" in xhr) {
-// XHR for Chrome/Firefox/Opera/Safari.
-xhr.open(method, url, true);
-} else if (typeof XDomainRequest != "undefined") {
-// XDomainRequest for IE.
-xhr = new XDomainRequest();
-xhr.open(method, url);
-} else {
-// CORS not supported.
-xhr = null;
-}
-return xhr;
-}
-// Helper method to parse the title tag from the response.
-function getTitle(text) {
-return text.match(\'<title>(.*)?</title>\')[1];
-}
-// Make the actual CORS request.
-function makeCorsRequest() {
-// This is a sample server that supports CORS.
-var url = \'https://eartrainer.app/melodic-production/js/midi.js\';
-var xhr = createCORSRequest(\'GET\', url);
-if (!xhr) {
-alert(\'CORS not supported\');
-return;
-}
-// Response handlers.
-xhr.onload = function() {
-var text = xhr.responseText;
-var title = getTitle(text);
-alert(\'Response from CORS request to \' + url + \': \' + title);
-};
-xhr.onerror = function() {
-alert(\'Woops, there was an error making the request.\');
-};
-xhr.send();
-}
-'

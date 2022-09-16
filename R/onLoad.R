8bf98d543d2f06c9c75a55af1d2ba39f839a8a5c
syntheso <sebsilas@gmail.com>
2022-09-16 16:32:02 +0200

    onLoad bug fix

--- R/onLoad.R
+++ R/onLoad.R
@@ -3,8 +3,8 @@
     prefix = "musicassessr-assets", # custom prefix that will be used to reference your directory
     directoryPath = system.file("www", package = "musicassessr") # path to resource in your package
   )
-  shiny::addResourcePath(
-    prefix = "item_banks", # custom prefix that will be used to reference your directory
-    directoryPath = system.file("extdata", package = 'Berkowitz')
-  )
+  # shiny::addResourcePath(
+  #   prefix = "item_banks", # custom prefix that will be used to reference your directory
+  #   directoryPath = system.file("extdata", package = 'Berkowitz')
+  # )
 }

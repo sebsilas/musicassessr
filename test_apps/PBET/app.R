df41f99f644cb98ef9d0d6161a992a89f381cf78
sebsilas <sebsilas@gmail.com>
2023-09-27 09:22:36 +0200

    Administrative changes

--- test_apps/PBET/app.R
+++ test_apps/PBET/app.R
@@ -1,7 +1,26 @@
 
 library(PBET)
+library(WJD)
 
-# setwd('test_apps/PBET')
+setwd('/Users/sebsilas/musicassessr/test_apps/PBET')
+
+# PBET_standalone(num_items = list("interval_perception" = 0L,
+#                                  "find_this_note" = 0L,
+#                                  "arrhythmic" = list("key_easy" = 1L,
+#                                                      "key_hard" = 1L),
+#                                  "rhythmic" = list("key_easy" = 1L,
+#                                                    "key_hard" = 1L),
+#                                  "wjd_audio" = list("key_easy" = 0L,
+#                                                     "key_hard" = 0L)),
+#                 skip_setup = F,
+#                 concise_wording = TRUE,
+#                 force_p_id_from_url = TRUE,
+#                 allow_any_p_id_url = TRUE,
+#                 logo = "musicassessr-assets/img/slice_the_pie_logo.jpg",
+#                 logo_width = "200px",
+#                 logo_height = "81px",
+#                 app_name = "PBET")
+
 
 PBET_standalone(num_items = list("interval_perception" = 0L,
                                  "find_this_note" = 0L,
@@ -12,12 +31,9 @@ PBET_standalone(num_items = list("interval_perception" = 0L,
                                  "wjd_audio" = list("key_easy" = 0L,
                                                     "key_hard" = 0L)),
                 skip_setup = F,
+                SNR_test = FALSE,
                 concise_wording = TRUE,
-                force_p_id_from_url = TRUE,
-                allow_any_p_id_url = TRUE,
-                logo = "musicassessr-assets/img/slice_the_pie_logo.jpg",
-                logo_width = "200px",
-                logo_height = "81px")
+                app_name = "PBET")
 
 
 # shiny::runApp("test_apps/PBET/app.R")

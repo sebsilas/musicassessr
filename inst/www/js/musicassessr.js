d146595b4e54d6ffa2ec5c0a100c0b5f98629191
sebsilas <sebsilas@gmail.com>
2023-11-13 15:24:29 +0100

    Begin s3 uploading functionality

--- inst/www/js/musicassessr.js
+++ inst/www/js/musicassessr.js
@@ -30,7 +30,7 @@ var recordkey;
 var file_url;
 var onsets_noteon_timecode = [];
 var stimulus_trigger_times = [];
-var upload_to_s3 = true;
+var upload_to_s3 = false;
 
 // functions
 
@@ -949,7 +949,7 @@ function stopAudioRecording() {
   if(upload_to_s3) {
     rec.exportWAV(upload_file_to_s3);
   } else {
-    rec.exportWAV(store_file_locally);
+    rec.exportWAV(process_file_on_server);
   }
 
 
@@ -1015,6 +1015,40 @@ function store_file_locally(blob) {
 }
 
 
+
+function process_file_on_server(blob) {
+
+	var xhr = new XMLHttpRequest();
+
+	var fd = new FormData();
+
+	fd.append("audio_data", blob, recordkey);
+
+	if (typeof shiny_app_name !== 'undefined') {
+	  fd.append("app_name", shiny_app_name);
+	  Shiny.setInputValue("shiny_app_name", shiny_app_name);
+	}
+
+	if(this.musicassessr_state === "production") {
+	  xhr.open("POST","/api/store_audio/",true); // production
+  } else {
+    console.log('Upload local...');
+    xhr.open("POST","http://localhost:3000/upload-audio",true); // local
+  }
+
+	xhr.send(fd);
+
+
+	xhr.onload = () => {
+	  console.log(xhr.responseText);
+		// call next page after credentials saved
+		spinner = document.getElementsByClassName("hollow-dots-spinner");
+		if(spinner[0] !== undefined) {
+		  spinner[0].style.display = "none";
+		}
+		file_is_ready = true;
+	};
+}
 
 function upload_file_to_s3(blob){
 
--- tests/testthat/test-DB_user.R
+++
@@ -1,7 +0,0 @@
-test_that("validate_user_entry_into_test works", {
-
-  tl <- psychTestR::join(psychTestR::one_button_page("Hi"))
-
-  expect_is(validate_user_entry_into_test(validate_user_entry_into_test = TRUE, elts = tl),
-            "list")
-})
